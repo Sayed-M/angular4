91925ad32314f8c6ba937e2e2e7694fe0258615b
Sayed-M <sayko102030@gmail.com>
2018-04-12 05:16:17 +0200

    git commit 'fix jquery'

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-signup',
@@ -10,6 +11,10 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // ================= Tabs ================
+    $('.main-content .left-content .form-table .tabs span').on('click', function () {
+      $(this).addClass('active').siblings().removeClass('active');
+    });
   }
 
 }
