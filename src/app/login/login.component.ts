67b08880b3b06699303679a97d5cb8189a42d194
Sayed-M <sayko102030@gmail.com>
2018-04-18 14:21:19 +0200

    git commit "login component show pass fix"

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,28 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // ================= SHOW PASSWORD ================
+    $('.password-input').each(function (index, input) {
+      let $input = $(input);
+      $('.form-table .show').click(function () {
+          let change = '';
+          if ($(this).html() === 'Show') {
+              $(this).html('Hide');
+              change = 'text';
+          } else {
+              $(this).html('Show');
+              change = 'password';
+          }
+          const rep = $('<input type="' + change + '" />')
+          .attr('id', $input.attr('id'))
+          .attr('name', $input.attr('name'))
+          .attr('class', $input.attr('class'))
+          .val($input.val())
+          .insertBefore($input);
+      $input.remove();
+      $input = rep;
+      });
+    });
   }
 
 }
