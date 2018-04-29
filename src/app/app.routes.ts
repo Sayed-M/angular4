d0bbc2d4be772b0a22ce41525173717429805ea2
Sayed-M <sayko102030@gmail.com>
2018-04-29 22:29:41 +0200

    git commit "redirectTo removed"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import {LoginComponent} from './login/login.component';
-import {Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { Step2SignupComponent } from './step2-signup/step2-signup.component';
 import { Step3SignupComponent } from './step3-signup/step3-signup.component';
@@ -13,5 +13,5 @@ export const master_routes: Routes = [
     {path: 'activate-account', component: ActivateAccountComponent},
     {path: 'login', component: LoginComponent},
     {path: 'profile', component: ProfileComponent},
-    {path: '**', redirectTo: '/signup'}
+    {path: '**', component: SignupComponent}
 ];
